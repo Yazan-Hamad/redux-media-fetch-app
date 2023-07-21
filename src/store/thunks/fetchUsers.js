820405be504c495a2b371597ad05089e38a8aac5
Yazan <53478218+Yazan-Hamad@users.noreply.github.com>
2023-07-21 05:20:03 +0300

    create fetchUsers thunk

---
+++ src/store/thunks/fetchUsers.js
@@ -0,0 +1,8 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const fetchUsers = createAsyncThunk("users/fetch", async ()=>{
+    const reponse = await axios.get("http://localhost:3005/users");
+    return reponse.data;
+});
+export {fetchUsers};
