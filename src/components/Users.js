f18885a287a4e04f195a0aae97c27e7250d00759
Yazan <53478218+Yazan-Hamad@users.noreply.github.com>
2023-07-21 06:01:28 +0300

    use dispatch functions

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,5 +1,24 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUsers } from "../store";
+
 function Users(){
-    return <div>Users</div>;
+    const dispatch = useDispatch();
+    const {isLoading, data, error} = useSelector((state)=>{
+        return state.users;
+    });
+    useEffect(()=>{
+        dispatch(fetchUsers());
+    },[dispatch]);
+
+    if (isLoading){
+        return <div>Loading..</div>
+    }
+    if (error){
+        return <div>Error..</div>
+    }
+
+    return <div>{data.length}</div>;
 }
 
 export default Users;
