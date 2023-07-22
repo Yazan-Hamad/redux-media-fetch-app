f11bcca1c394d58399d40e3bed131ed701305098
Yazan <53478218+Yazan-Hamad@users.noreply.github.com>
2023-07-23 00:31:58 +0300

    configure RTLQ api for albims

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/usersSlice";
+import { albumsApi } from "./apis/albums";
+import { setupListeners } from "@reduxjs/toolkit/query";
 
 export const store = configureStore({
     reducer:
     {
-        users: usersReducer
+        users: usersReducer,
+        [albumsApi.reducerPath]: albumsApi.reducer
+    },
+    middleware: (getDefaultMiddleware)=>{
+        return getDefaultMiddleware().concat(albumsApi.middleware);
     }
 });
+
+setupListeners(store.dispatch);
 
 export * from "./thunks/fetchUsers";
 export * from "./thunks/createUser";
 export * from "./thunks/removeUser";
+export { useFetchAlbumsQuery, useCreateAlbumMutation } from "./apis/albums";
