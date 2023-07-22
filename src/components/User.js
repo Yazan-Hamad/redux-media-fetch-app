981ba68e9e085af0d983c5f68321a3a7da535cd0
Yazan <53478218+Yazan-Hamad@users.noreply.github.com>
2023-07-22 06:46:11 +0300

    create and configure Expandable component

--- src/components/User.js
+++ src/components/User.js
@@ -1,26 +1,29 @@
-import {GoTrash} from "react-icons/go";
+import { GoTrash } from "react-icons/go";
 import Button from "./Button";
 import useThunk from "../hooks/useThunk";
 import { removeUser } from "../store";
+import Expandable from "./Expandable";
+import { Fragment } from "react";
+import Albums from "./Albums";
+
 function User({ user }) {
-    const [doRemoveUser, isLoading, error] = useThunk(removeUser);
-    const onRemoveUser = () =>{
-        doRemoveUser(user);
-    }
-  return (
-    <div className="mb-2 border rounded">
-            <div className="flex p-2 justify-between items-center cursor-pointer">
-            <div className="flex flex-row items-center justify-between">
-                <Button className="mr-3" loading={isLoading} onClick={onRemoveUser} >
-                    <GoTrash/>
-                </Button>
-                
-                {error && <div>Error Deleting User..</div>}
-              {user.name}
-            </div>
-            </div>
-    </div>
+  const [doRemoveUser, isLoading, error] = useThunk(removeUser);
+  const onRemoveUser = () => {
+    doRemoveUser(user);
+  };
+  const header = (
+    <Fragment>
+      <Button className="mr-3" loading={isLoading} onClick={onRemoveUser}>
+        <GoTrash />
+      </Button>
+      {error && <div>Error Deleting User..</div>}
+      {user.name}
+    </Fragment>
   );
+  return (
+  <Expandable header={header}>
+    <Albums user={user} />
+  </Expandable>);
 }
 
 export default User;
